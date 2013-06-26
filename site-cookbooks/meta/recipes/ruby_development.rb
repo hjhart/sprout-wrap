04fdf7042369ac2f71b795dab6a4667464519be9
James Hart <hjhart@gmail.com>
2013-06-25 23:17:10 -0700

    removed unnecessary apps.

--- site-cookbooks/meta/recipes/ruby_development.rb
+++ site-cookbooks/meta/recipes/ruby_development.rb
@@ -1,6 +1,5 @@
 include_recipe "pivotal_workstation::rvm"
 include_recipe "pivotal_workstation::gem_setup"
-include_recipe "pivotal_workstation::mysql"
 include_recipe "pivotal_workstation::postgres"
 include_recipe "pivotal_workstation::rubymine"
 include_recipe "pivotal_workstation::rubymine_preferences_pivotal"
