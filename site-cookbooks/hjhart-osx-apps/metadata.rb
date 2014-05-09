ab9042bc64da666f5788c533733c47c029b735e9
James Hart <hjhart@gmail.com>
2014-05-09 14:54:49 -0700

    Remove dependency on pivotal_workstation

--- site-cookbooks/hjhart-osx-apps/metadata.rb
+++ site-cookbooks/hjhart-osx-apps/metadata.rb
@@ -4,6 +4,5 @@ description       'Some more apps that I use personally'
 version           '0.1.0'
 supports          'mac_os_x'
 
-depends           'pivotal_workstation'
 depends           'sprout-osx-settings'
 depends           'sprout-osx-apps'
