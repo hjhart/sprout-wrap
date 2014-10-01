c4e854287e74b5a90833b32290d4c56f31eb09a1
James Hart <hjhart@gmail.com>
2014-10-01 13:08:40 -0700

    Okay, cool beans

--- site-cookbooks/hjhart-osx-settings/files/default/modify_path.bash
+++ site-cookbooks/hjhart-osx-settings/files/default/modify_path.bash
@@ -1,1 +1,19 @@
 PATH=$PATH:/usr/local/sbin
+
+
+alias 'gst'='git status'
+alias 'gpr'='git pull --rebase'
+alias 'gprs'='git stash && git pull --rebase && git stash pop'
+
+alias 'gs'='git status'
+alias 'gc'='git checkout'
+alias 'gd'='git diff'
+alias 'gi'='git commit'
+alias 'gm'='git pair-commit -m'
+alias 'gl'='git log'
+alias 'gpp'='git pull && git push'
+alias 'ga'='git add'
+alias 'gaa'='git add -A'
+alias 'gap'='git add -p'
+
+
